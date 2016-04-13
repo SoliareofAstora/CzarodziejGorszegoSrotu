7005533a7763360d61c67cc70095f38bcbaed398
SoliareOfAstora <piotr1kucharski@gmail.com>
2016-04-13 19:18:41 +0200

    4

---
+++ CzarodziejMain/Cośtam/KillZone.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillZone : MonoBehaviour {
+
+    public BoxCollider box;
+	// Use this for initialization
+	void Start () {
+        box = GetComponent<BoxCollider>();
+	}
+	public void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
