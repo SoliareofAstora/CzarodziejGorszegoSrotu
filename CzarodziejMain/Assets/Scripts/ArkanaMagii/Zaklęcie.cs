ce612a4c7b5d989f09ce5a4168b947c231441716
SoliareOfAstora <piotr1kucharski@gmail.com>
2016-05-31 17:19:27 +0200

    wracamy do roboty. I czekam...

--- CzarodziejMain/Assets/Scripts/ArkanaMagii/Zaklęcie.cs
+++ CzarodziejMain/Assets/Scripts/ArkanaMagii/Zaklęcie.cs
@@ -1,11 +1,19 @@
 ﻿using System.ComponentModel.Design.Serialization;
+using MyClock;
 using rodzajezaklęć;
 
 namespace zaklęcie
 {
     public class Zaklęcie:PodstawoweZaklęcie
     {
-         public RodzajeZaklęć GetTypeZaklęć()
+        private RodzajeZaklęć rodzaj;
+        public string Nazwa;
+        public int Obrażenia;
+        public float Szybkość;
+        private float CzasOdnowienia;
+        
+
+        public RodzajeZaklęć GetTypeZaklęć()
          {
             return RodzajeZaklęć.KulaOgnia;
          }
---
+++ CzarodziejMain/Assets/Scripts/PLAYER.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PLAYER : MonoBehaviour {
+    public static PLAYER instance;
+    private Animator Anim;
+    private Transform nos;
+
+    private BoxCollider2D box;
+    public PLAYER()
+    {
+        instance = this;
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+    void OnGUI()
+    {
+        MusicManager.play("CzarnaMsza", 1.0f, 1.0f);
+        GameObject player = MusicManager.getMusicEmitter();
+    }
+    void Awake()
+    {
+        GameObject player = MusicManager.getMusicEmitter();
+        MusicManager.play("CzarnaMsza", 1.0f, 1.0f);
+        nos = GetComponentInChildren<Transform>();
+        Księga = new BookCzarów[2];
+        Księga[0] = new BookCzarów("Fire", 12, 5, 1, "FBP");
+        Księga[1] = new BookCzarów("Thunder", 12, 5, 1, "ThunderBall");
+    }
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
