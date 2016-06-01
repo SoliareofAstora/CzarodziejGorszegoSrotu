8da310eecabe56ac63f3a37ff77af0ff44c9df67
SoliareOfAstora <piotr1kucharski@gmail.com>
2016-06-01 11:59:00 +0200

    qwe

--- CzarodziejMain/Assets/Scripts/PLAYER.cs
+++ CzarodziejMain/Assets/Scripts/PLAYER.cs
@@ -8,36 +8,35 @@ public class PLAYER : MonoBehaviour
     private BoxCollider2D box;
     private Transform nos;
 
-    public PLAYER() { instance = this; }
-
-    // Use this for initialization
-    private void OnGUI()
+    public PLAYER()
     {
-        //MusicManager.play("CzarnaMsza", 1.0f, 1.0f);
+        instance = this;
     }
 
-    private void Awake() {//MusicManager.play("CzarnaMsza", 1.0f, 1.0f);
+    private void Awake()
+    {
+//MusicManager.play("CzarnaMsza", 1.0f, 1.0f);
     }
 
-    private void Start() { Anim = GetComponent<Animator>(); }
+    private void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     private void Update()
     {
-
+        FollowMouse();
         if (Input.GetKey(KeyCode.Escape))
         {
             Application.Quit();
         }
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-        }
+        
     }
 
     private void FollowMouse()
     {
-        var lookPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)) -
-                      transform.position;
+        var lookPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)) - transform.position;
         transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(lookPos.y, lookPos.x)*Mathf.Rad2Deg - 90, Vector3.forward);
     }
 }
---
+++ CzarodziejMain/Assets/Scripts/Sterowanie.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sterowanie
+{
+    /*
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Escape)) {
+            Application.Quit();
+        }
+        resetAnimations();
+        if (Input.GetKeyDown(KeyCode.Q)
+            || Input.GetKeyDown(KeyCode.Mouse0))
+            CastSpell(0);
+        if (Input.GetKeyDown(KeyCode.W)
+            || Input.GetKeyDown(KeyCode.Mouse1))
+            CastSpell(1);
+        throwSpells();
+        */
+
+}
+    
