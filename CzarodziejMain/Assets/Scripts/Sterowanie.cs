fcd617563c66407efee6b909d76b1afb805c1981
SoliareOfAstora <piotr1kucharski@gmail.com>
2016-06-01 12:28:13 +0200

    GitIgnore

--- CzarodziejMain/Assets/Scripts/Sterowanie.cs
+++ CzarodziejMain/Assets/Scripts/Sterowanie.cs
@@ -3,21 +3,28 @@ using System.Collections;
 
 public class Sterowanie
 {
+    private KeyCode sterowanie;
+
+    public KeyCode Dawajto()
+    {
+        return KeyCode.A;
+    }
+
     /*
 
-    void Update() {
-        if (Input.GetKey(KeyCode.Escape)) {
-            Application.Quit();
-        }
-        resetAnimations();
-        if (Input.GetKeyDown(KeyCode.Q)
-            || Input.GetKeyDown(KeyCode.Mouse0))
-            CastSpell(0);
-        if (Input.GetKeyDown(KeyCode.W)
-            || Input.GetKeyDown(KeyCode.Mouse1))
-            CastSpell(1);
-        throwSpells();
-        */
+void Update() {
+   if (Input.GetKey(KeyCode.Escape)) {
+       Application.Quit();
+   }
+   resetAnimations();
+   if (Input.GetKeyDown(KeyCode.Q)
+       || Input.GetKeyDown(KeyCode.Mouse0))
+       CastSpell(0);
+   if (Input.GetKeyDown(KeyCode.W)
+       || Input.GetKeyDown(KeyCode.Mouse1))
+       CastSpell(1);
+   throwSpells();
+   */
 
 }
     
