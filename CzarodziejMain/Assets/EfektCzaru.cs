25ccd408029a01770c83eba5bb78adf977ad2d3f
SoliareOfAstora <piotr1kucharski@gmail.com>
2016-06-04 22:57:19 +0200

    Mamy Wybuch! Działamy z czarami :smile:

---
+++ CzarodziejMain/Assets/EfektCzaru.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EfektCzaru : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
